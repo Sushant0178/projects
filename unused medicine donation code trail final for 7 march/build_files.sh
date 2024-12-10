300e30225ddc0eaacf9255d429fa8bb887acadcd
sushant kokate <91862793+Sushant0178@users.noreply.github.com>
2024-12-10 23:56:37 +0530

    ok

--- unused medicine donation code trail final for 7 march/build_files.sh
+++ unused medicine donation code trail final for 7 march/build_files.sh
@@ -4,6 +4,6 @@ echo "BUILD START"
 python3.9 -m pip install -r requirements.txt
 
 # Collect static files, clear old static files before collecting new ones
-python3.9 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
 
 echo "BUILD END"
